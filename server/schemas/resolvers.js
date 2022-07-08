bb79bfbd7494bde1e0e72a72fe61228d20b6a4b1
Brainybrian316 <brainybrian316@gmail.com>
2022-07-08 15:52:41 -0400

    middleware

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,5 +1,7 @@
 const { User, Thought } = require('../models');
 const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../utils/auth');
+
 
 
 const resolvers = {
@@ -26,12 +28,24 @@ user: async (parent, { username }) => {
     .populate('friends')
     .populate('thoughts');
 },
+me: async (parent, args, context) => {
+  if (context.user) {
+    const userData = await User.findOne({ _id: context.user._id })
+      .select('-__v -password')
+      .populate('thoughts')
+      .populate('friends');
+
+      return userData;
+  }
+  throw new AuthenticationError('Not logged in');
+}
 },
 Mutation: {
   addUser: async (parent, args) => {
     const user = await User.create(args);
+    const token = signToken(user);
   
-    return user;
+    return { token, user };
   },
   login: async (parent, { email, password }) => {
     const user = await User.findOne({ email });
@@ -46,7 +60,8 @@ Mutation: {
       throw new AuthenticationError('Incorrect credentials');
     }
   
-    return user;
+    const token = signToken(user);
+    return { token, user };
   }
 }
 };
---
+++ server/utils/auth.js
@@ -0,0 +1,23 @@
+authMiddleware = function({ req }) {
+  let token = req.body.token || req.query.token || req.headers.authorization;
+
+  if (req.headers.authorization) {
+    token = token
+      .split(' ')
+      .pop()
+      .trim();
+  }
+
+  if (!token) {
+    return req;
+  }
+
+  try {
+    const { date } = jwt.verify(token, secret, { maxAge: expiration });
+    req.user = data;
+  } catch {
+    console.log('Invalid token');
+  }
+
+  return req;
+}
