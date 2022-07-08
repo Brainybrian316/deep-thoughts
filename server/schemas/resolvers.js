5549b5a37b11591ea63528d7984bba5d0a5220ea
Brainybrian316 <brainybrian316@gmail.com>
2022-07-08 13:07:22 -0400

    user and login route

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,4 +1,5 @@
 const { User, Thought } = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
 
 
 const resolvers = {
@@ -26,6 +27,28 @@ user: async (parent, { username }) => {
     .populate('thoughts');
 },
 },
+Mutation: {
+  addUser: async (parent, args) => {
+    const user = await User.create(args);
+  
+    return user;
+  },
+  login: async (parent, { email, password }) => {
+    const user = await User.findOne({ email });
+  
+    if (!user) {
+      throw new AuthenticationError('Incorrect credentials');
+    }
+  
+    const correctPw = await user.isCorrectPassword(password);
+  
+    if (!correctPw) {
+      throw new AuthenticationError('Incorrect credentials');
+    }
+  
+    return user;
+  }
+}
 };
 
 module.exports = resolvers;
