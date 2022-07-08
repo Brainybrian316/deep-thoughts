d9eae00d699b46209e54a8fd4ef3955064c3988f
Brainybrian316 <brainybrian316@gmail.com>
2022-07-08 16:16:59 -0400

    login

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,20 +1,6 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-  type Thought {
-    _id: ID
-    thoughtText: String
-    createdAt: String
-    username: String
-    reactionCount: Int
-    reactions: [Reaction]
-  }
-  type Reaction {
-    _id: ID
-    reactionBody: String
-    createdAt: String
-    username: String
-  }
   type User {
     _id: ID
     username: String
@@ -23,6 +9,28 @@ const typeDefs = gql`
     thoughts: [Thought]
     friends: [User]
   }
+
+  type Thought {
+    _id: ID
+    thoughtText: String
+    createdAt: String
+    username: String
+    reactionCount: Int
+    reactions: [Reaction]
+  }
+
+  type Reaction {
+    _id: ID
+    reactionBody: String
+    createdAt: String
+    username: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
   type Query {
     me: User
     users: [User]
@@ -30,14 +38,14 @@ const typeDefs = gql`
     thoughts(username: String): [Thought]
     thought(_id: ID!): Thought
   }
+
   type Mutation {
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
+    addThought(thoughtText: String!): Thought
+    addReaction(thoughtId: ID!, reactionBody: String!): Thought
+    addFriend(friendId: ID!): User
   }
-  type Auth {
-    token: ID!
-    user: User
-  }
-  `;
+`;
 
 module.exports = typeDefs;
