076de1529d176dd00c7452bb55ba37e1371eb388
Brainybrian316 <brainybrian316@gmail.com>
2022-07-08 21:53:46 -0400

    render single thought

--- client/src/App.js
+++ client/src/App.js
@@ -9,12 +9,11 @@ import SingleThought from './pages/SingleThought';
 import Profile from './pages/Profile';
 import Signup from './pages/Signup';
 
-
 import Home from './pages/Home';
 
 const httpLink = createHttpLink({
   uri: '/graphql',
-})
+});
 
 const client = new ApolloClient({
   link: httpLink,
@@ -25,18 +24,24 @@ function App() {
   return (
     <ApolloProvider client={client}>
       <Router>
-      <div className="flex-column justify-flex-start min-100-vh">
-        <Header />
-        <div className="container">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/thought" element={<SingleThought />} />
-          </Routes>
-        </div>
-        <Footer />
+        <div className="flex-column justify-flex-start min-100-vh">
+          <Header />
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<Signup />} />
+              {/* <Route path="/profile" element={<Profile />} />
+            <Route path="/thought" element={<SingleThought />} /> */}
+              <Route path="/profile">
+                <Route path=":username" element={<Profile />} />
+                <Route path="" element={<Profile />} />
+              </Route>
+              <Route path="/thought/:id" element={<SingleThought />} />
+              <Route path="*" element={<NoMatch />} />
+            </Routes>
+          </div>
+          <Footer />
         </div>
       </Router>
     </ApolloProvider>
